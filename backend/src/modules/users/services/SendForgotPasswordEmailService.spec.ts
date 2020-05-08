77dc10913faf3e78d9add82b3579e4d3e14744f3
marcelokanu <marcelokanu@gmail.com>
2020-05-08 09:18:18 -0400

    test of recovery password  created

---
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,31 @@
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+
+// import AppError from '@shared/errors/AppError';
+import SendForgotPasswordEmailService from '@modules/users/services/SendForgotPasswordEmailService';
+
+describe('SendForgotPasswordEmail', () => {
+  it('should be able to recover the password using the email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+    await fakeUsersRepository.create({
+      name: 'John Do',
+      email: 'johndo@example.com',
+      password: '123456',
+    });
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+    );
+
+    await sendForgotPasswordEmail.execute({
+      email: 'johndoe@example.com',
+    });
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+});
--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -3,6 +3,8 @@ import { container } from 'tsyringe';
 import IStorageProvider from '@shared/container/providers/StorageProviders/models/IStorageProvider';
 import DiskStorageProvider from '@shared/container/providers/StorageProviders/implementations/DiskStorageProvider';
 
+// import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
